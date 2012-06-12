1975d31ef947355198089d30196812890ec0dc35
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2012-06-12 14:05:52 +0200

    renamed uoo -> umf again

--- umf.lua
+++ umf.lua
@@ -4,59 +4,76 @@
 require "utils"
 module("umf", package.seeall)
 
---- Everything is derived from Object.
-Object={}
-Object_mt = { __index=Object }
+Object={}      -- Everything is derived from class Object.
+Object_cops={} -- Methods of class
+Object_iops={} -- Methods of instances of class
 
-function Object:new(t)
+-- Object methods
+function Object_iops:class() return getmetatable(self).class end
+function Object_iops:type() return 'instance' end
+function Object_iops:tostring() return "instance of class "..self:class():classname() end
+
+-- Class methods
+function Object_cops:super() return getmetatable(self).super end
+function Object_cops:classname() return getmetatable(self).classname end
+function Object_cops:tostring() return 'Class '.. self:classname() end
+function Object_cops:type() return 'class' end
+function Object_cops:cops() return getmetatable(self).__index end
+function Object_cops:iops() return getmetatable(self).iops end
+function Object_cops:addMethod(k,m) self:iops()[k]=m end
+
+function Object_cops:new(t)
    local newobj = t or {}
-   setmetatable(newobj, Object_mt)
+   setmetatable(newobj, { class=Object, __index=Object_iops, __tostring=Object_iops.tostring })
    return newobj
 end
 
-function Object:type() return 'Object' end
-function Object:tostring() return "Class '"..self:type().."'" end
-function Object:class() return Object end
-function Object:super() return false end
-setmetatable(Object, { __call=Object.new, __tostring=Object.tostring }) -- just to be consistent
+setmetatable(Object, { classname='Object', iops=Object_iops, super=false,
+		       __index=Object_cops, __newindex=Object_cops.addMethod,
+		       __tostring=Object_cops.tostring, __call=Object_cops.new } )
 
 --- Create a new class.
 function class(name, base)
    base = base or Object
    local klass = {}
-   local klass_mt = { __index = klass }
-
-   function klass:super() return base end
+   local klass_iops={}
+   local klass_cops={}
 
    -- Create Constructor
-   function klass:new(t)
+   function klass_cops:new(t)
       local newobj = t or {}
-      setmetatable( newobj, klass_mt)
+      setmetatable( newobj, { class=klass, __index=klass_iops })
+      setmetatable( klass_iops, { __index=base:iops() })
       return newobj
    end
 
-   -- return name of class
-   function klass:type() return name end
-
-   -- Return class object of instance.
-   function klass:class() return klass end
-
-   -- Enable inheritance + "call" constructors
-   setmetatable(klass, { __index=base, __call=klass.new } )
+   setmetatable(klass, { classname=name, iops=klass_iops, super=base,
+			 __index=klass_cops, __newindex=Object_cops.addMethod, __call=klass_cops.new  })
+   setmetatable(klass_cops, { __index=base:cops() })
    return klass
 end
 
---- Check if one class is equal or a subclass of another.
-function subclass_of(sc, c)
-   if not sc then return false end
-   if sc==c then return true
-   else return  subclass_of(sc:super(), c) end
+function uoo_type(x)
+   if not x then return false end
+   local mt = getmetatable(x)
+   if mt and mt.class then return 'instance'
+   elseif mt and mt.classname then return 'class' end
+   return false
 end
 
---- Check if obj is an instance of class.
+function subclass_of(subklass, klass)
+   function sc_of(sc, k)
+      if not sc then return false end
+      if sc==k then return true end
+      return sc_of(sc:super(), k)
+   end
+   assert(uoo_type(subklass)=='class', "subclass_of: first argument not a class")
+   assert(uoo_type(klass)=='class', "subclass_of: second argument not a class")
+   return sc_of(subklass, klass)
+end
+
 function instance_of(klass, obj)
-   if not klass then return false end
-   local c = obj:class()
-   if c == klass then return true
-   else return subclass_of(c, klass) end
+   assert(uoo_type(klass)=='class', "instance_of: first argument not a class")
+   assert(uoo_type(obj)=='instance', "instance_of: second argument not an instance")
+   return subclass_of(obj:class(), klass)
 end
---
+++ uoo-test.lua
@@ -0,0 +1,47 @@
+
+require "umf"
+
+Animal=umf.class("Animal")
+Mammal=umf.class("Mammal", Animal)
+Human=umf.class("Human", Mammal)
+
+function Mammal:name()
+   print ("mammal name: ", self.name)
+end
+
+function Mammal:speak()
+   print("wuff")
+end
+
+function Human:speak()
+   print("haloelle says ".. self.name)
+end
+
+a1=Animal{}
+m1=Mammal{ name="bello" }
+h1=Human{ name="Jeff"}
+
+m1:speak()
+h1:speak()
+
+
+print("instance_of(a1, Animal):", umf.instance_of(Animal, a1))
+print("instance_of(m1, Animal):", umf.instance_of(Animal, m1))
+print("instance_of(h1, Animal):", umf.instance_of(Animal, h1))
+
+print("instance_of(a1, Mammal):", umf.instance_of(Mammal, a1))
+print("instance_of(m1, Mammal):", umf.instance_of(Mammal, m1))
+print("instance_of(h1, Mammal):", umf.instance_of(Mammal, h1))
+
+print("instance_of(a1, Human): ", umf.instance_of(Human, a1))
+print("instance_of(m1, Human): ", umf.instance_of(Human, m1))
+print("instance_of(h1, Human): ", umf.instance_of(Human, h1))
+
+print("instance_of(a1, Object):", umf.instance_of(umf.Object, a1))
+print("instance_of(m1, Object):", umf.instance_of(umf.Object, m1))
+print("instance_of(h1, Object):", umf.instance_of(umf.Object, h1))
+
+-- print("instance_of(Animal, Object):", umf.instance_of(Animal, uoo.Object))
+-- print("instance_of(Mammal, Object):", umf.instance_of(Mammal, uoo.Object))
+-- print("instance_of(Human, Object):", umf.instance_of(Human, uoo.Object))
+
